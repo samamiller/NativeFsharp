3c9dc1b4742ec81293d82ebc9cc7502f7caef144
Sam Miller <samamiller@users.noreply.github.com>
2022-06-22 16:44:41 -0600

    Add project files.

---
+++ Program.fs
@@ -0,0 +1,22 @@
+﻿open Windows.Win32
+open System
+open System.Diagnostics
+
+let mutable bitmapInfo = Graphics.Gdi.BITMAPINFO()
+bitmapInfo.bmiHeader.biPlanes <- 1us
+bitmapInfo.bmiHeader.biBitCount <- 32us
+bitmapInfo.bmiHeader.biSize <- uint sizeof<Graphics.Gdi.BITMAPINFOHEADER>
+let mutable bitmap = NativeInterop.NativePtr.stackalloc<byte>(100)
+let mutable bitmapMemory = bitmap |> NativeInterop.NativePtr.toVoidPtr
+let mutable bitmapHandle = Graphics.Gdi.HBITMAP()
+let mutable bitmapDeviceContext = Graphics.Gdi.HDC()
+
+let win32ResizeDIBSection width height =
+    bitmapInfo.bmiHeader.biWidth <- width
+    bitmapInfo.bmiHeader.biHeight <- height
+    bitmapHandle <- PInvoke.CreateDIBSection(bitmapDeviceContext, &&bitmapInfo, Graphics.Gdi.DIB_USAGE.DIB_RGB_COLORS, &&bitmapMemory, Foundation.HANDLE 0n, 0u)
+
+[<EntryPoint>]
+let main _ =
+    
+    0
